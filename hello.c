9f7d845cd438804baa8807b95839d38d5841e434
sugar@2955134423.com <sugar@WIN-Sugar>
2025-07-30 15:58:46 +0800

    添加了新的hello world

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "hello.h"
+void print_hello(){
+    printf("hello world\n");
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello.h"
+int main(int argc, char const *argv[])
+{
+    print_hello();
+    return 0;
+}
